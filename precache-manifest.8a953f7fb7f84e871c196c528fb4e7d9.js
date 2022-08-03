138b6fdd372e829060be608a4ab28f55ceb28bf8
10cyrilc <10cyrilc@gmail.com>
2022-08-03 20:19:30 +0530

    Updates

---
+++ precache-manifest.8a953f7fb7f84e871c196c528fb4e7d9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ce3175d440adeba4c4f",
+    "url": "/portfolio/static/css/main.af7916a8.chunk.css"
+  },
+  {
+    "revision": "4ce3175d440adeba4c4f",
+    "url": "/portfolio/static/js/main.4ce3175d.chunk.js"
+  },
+  {
+    "revision": "17199e8163d6ea006b29",
+    "url": "/portfolio/static/css/1.4c8e6ca8.chunk.css"
+  },
+  {
+    "revision": "17199e8163d6ea006b29",
+    "url": "/portfolio/static/js/1.17199e81.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "8a284cfc614e376a387761418cfe71ed",
+    "url": "/portfolio/static/media/logo.8a284cfc.png"
+  },
+  {
+    "revision": "2565a7890034c475dc6ee46fbc2b53b6",
+    "url": "/portfolio/static/media/cyril1.2565a789.jpg"
+  },
+  {
+    "revision": "b8f0c21b14128367f6733fbc20c7d5b2",
+    "url": "/portfolio/static/media/react.b8f0c21b.svg"
+  },
+  {
+    "revision": "ff1e9becefc5d60043e6781148fd09ce",
+    "url": "/portfolio/static/media/pymongo.ff1e9bec.svg"
+  },
+  {
+    "revision": "dc4613234a87a74e984eab894741e09f",
+    "url": "/portfolio/static/media/pyrogram.dc461323.svg"
+  },
+  {
+    "revision": "102c66c4f4781b5749c3013d650199f9",
+    "url": "/portfolio/static/media/fastapi.102c66c4.svg"
+  },
+  {
+    "revision": "eb7586536a32e8955f825918f8801a08",
+    "url": "/portfolio/static/media/not-found.eb758653.svg"
+  },
+  {
+    "revision": "125ee956a057833ca0edd5721867c3d6",
+    "url": "/portfolio/index.html"
+  }
+];
