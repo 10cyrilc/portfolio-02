8c3bb952ece1389391b0113247b34803e784c192
10cyrilc <10cyrilc@gmail.com>
2022-08-04 13:07:23 +0530

    Updates

---
+++ precache-manifest.fa6a707a66131c92328a3daff80d0636.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0a496071b9daff23032",
+    "url": "/static/css/main.8eb6b689.chunk.css"
+  },
+  {
+    "revision": "a0a496071b9daff23032",
+    "url": "/static/js/main.a0a49607.chunk.js"
+  },
+  {
+    "revision": "adb47f0b0554394cf64c",
+    "url": "/static/css/1.4c8e6ca8.chunk.css"
+  },
+  {
+    "revision": "adb47f0b0554394cf64c",
+    "url": "/static/js/1.adb47f0b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a284cfc614e376a387761418cfe71ed",
+    "url": "/static/media/logo.8a284cfc.png"
+  },
+  {
+    "revision": "2565a7890034c475dc6ee46fbc2b53b6",
+    "url": "/static/media/cyril1.2565a789.jpg"
+  },
+  {
+    "revision": "b8f0c21b14128367f6733fbc20c7d5b2",
+    "url": "/static/media/react.b8f0c21b.svg"
+  },
+  {
+    "revision": "ff1e9becefc5d60043e6781148fd09ce",
+    "url": "/static/media/pymongo.ff1e9bec.svg"
+  },
+  {
+    "revision": "dc4613234a87a74e984eab894741e09f",
+    "url": "/static/media/pyrogram.dc461323.svg"
+  },
+  {
+    "revision": "102c66c4f4781b5749c3013d650199f9",
+    "url": "/static/media/fastapi.102c66c4.svg"
+  },
+  {
+    "revision": "eb7586536a32e8955f825918f8801a08",
+    "url": "/static/media/not-found.eb758653.svg"
+  },
+  {
+    "revision": "a96ca18d65fc3727c68ce97c302627cb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fccdf65c3a4fb615eafb62d23fbb7857.js"
+  "/precache-manifest.fa6a707a66131c92328a3daff80d0636.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
