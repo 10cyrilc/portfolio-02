7592d171080ef487702b06f314f881056012ffa6
10cyrilc <10cyrilc@gmail.com>
2023-01-06 21:30:40 +0530

    Updates

---
+++ precache-manifest.b4b00ebb3686e30bd9b0fe1563197b4e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "24969a9817084fdc4a76",
+    "url": "/static/css/main.4b641335.chunk.css"
+  },
+  {
+    "revision": "24969a9817084fdc4a76",
+    "url": "/static/js/main.24969a98.chunk.js"
+  },
+  {
+    "revision": "1667d421bb9ca60a2ba6",
+    "url": "/static/css/1.19ad8b3b.chunk.css"
+  },
+  {
+    "revision": "1667d421bb9ca60a2ba6",
+    "url": "/static/js/1.1667d421.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a284cfc614e376a387761418cfe71ed",
+    "url": "/static/media/logo.8a284cfc.png"
+  },
+  {
+    "revision": "709c9a56224bc805c03713777407eff5",
+    "url": "/static/media/cyril1.709c9a56.webp"
+  },
+  {
+    "revision": "b8f0c21b14128367f6733fbc20c7d5b2",
+    "url": "/static/media/react.b8f0c21b.svg"
+  },
+  {
+    "revision": "52032216199f24673d076d70b347ae01",
+    "url": "/static/media/tkinter.52032216.webp"
+  },
+  {
+    "revision": "dc4613234a87a74e984eab894741e09f",
+    "url": "/static/media/pyrogram.dc461323.svg"
+  },
+  {
+    "revision": "102c66c4f4781b5749c3013d650199f9",
+    "url": "/static/media/fastapi.102c66c4.svg"
+  },
+  {
+    "revision": "105e378eb46ba8542e9c380a953cec67",
+    "url": "/static/media/mongo.105e378e.webp"
+  },
+  {
+    "revision": "e361654e04c34e0f266bd61bdfdfd00f",
+    "url": "/static/media/Axios.e361654e.webp"
+  },
+  {
+    "revision": "8e90e03463c957f6c512923a1d21773b",
+    "url": "/static/media/react-native.8e90e034.webp"
+  },
+  {
+    "revision": "a6e39fb5526ceeb8351b3ddbd71dcee1",
+    "url": "/static/media/JWT.a6e39fb5.svg"
+  },
+  {
+    "revision": "06311bbe95a766fb9defcb5ea722ecb5",
+    "url": "/static/media/random-joke.06311bbe.webp"
+  },
+  {
+    "revision": "b50e4ed55729d38b0f4736feb3eccc76",
+    "url": "/static/media/cardamom-price.b50e4ed5.webp"
+  },
+  {
+    "revision": "c2d85e0fe44d65d393440b417cb4e1fa",
+    "url": "/static/media/3d-mbc.c2d85e0f.webp"
+  },
+  {
+    "revision": "27396f95f13bc62e60fa441203ec57cb",
+    "url": "/static/media/shop-catalogue.27396f95.webp"
+  },
+  {
+    "revision": "6c0f3ca312b25a23455274c9cc48beef",
+    "url": "/static/media/thandava.6c0f3ca3.webp"
+  },
+  {
+    "revision": "eb7586536a32e8955f825918f8801a08",
+    "url": "/static/media/not-found.eb758653.svg"
+  },
+  {
+    "revision": "9185bb588aa4f5b4e72409b5fd899fba",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7651010c3a49bcfab90b687f6a6c2c11.js"
+  "/precache-manifest.b4b00ebb3686e30bd9b0fe1563197b4e.js"
 );
 
 workbox.clientsClaim();
